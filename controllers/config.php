b506016794d8d3c0516a36f7281f39eab2592182
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2022-11-24 22:59:48 -0500

    Deployment

--- controllers/config.php
+++ controllers/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST', 'zw.interactiva.net.co:2083/cpsess5022288455/3rdparty/phpMyAdmin/index.php');          // Set database host
+define('DB_HOST', 'zw.interactiva.net.co:174.142.60.54');          // Set database host
 define('DB_USER', 'jonat5268_admin');             // Set database user
 define('DB_PASS', 'oix&kLop*zWN');             // Set database password
 define('DB_NAME', 'jonat5268_carpart2022');        // Set database name
