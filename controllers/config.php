f6068a4dc48543bdaaead1a0112858d8a4b172ca
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2022-12-02 22:08:59 -0500

    railway

--- controllers/config.php
+++ controllers/config.php
@@ -2,5 +2,5 @@
 
 define('DB_HOST', 'containers-us-west-39.railway.app:6195');          // Set database host
 define('DB_USER', 'root');             // Set database user
-define('DB_PASS', 'DSOQKX5rW8lwVB6w2wSHs');             // Set database password
+define('DB_PASS', 'SOQKX5rW8lwVB6w2wSHs');             // Set database password
 define('DB_NAME', 'railway');        // Set database name
