0d92afe27eaf9120991e5855e2f01b520303b42c
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2022-12-03 00:00:56 -0500

    railway

--- controllers/functions.php
+++ controllers/functions.php
@@ -111,7 +111,7 @@ function make_date()
   IntlDateFormatter::FULL
 );
 $fmt->setPattern("yyyy-MM-dd HH:mm:ss");
-echo $fmt->format(new DateTime());
+echo $fmt->format(DateTime());
 }
 /*--------------------------------------------------------------*/
 /* Function for  Readable date time
