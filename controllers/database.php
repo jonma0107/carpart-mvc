730a2ccff167cc4c2d5d9cad94314e18d075f12a
Jonathan Meza (SENA) <jduplantier44@gmail.com>
2023-04-15 13:13:37 -0500

    credentials for DB

--- controllers/database.php
+++ controllers/database.php
@@ -21,7 +21,7 @@ class MySqli_DB
     if (!$this->con) {
       die(" Database connection failed:" . mysqli_connect_error());
     } else {
-      $select_db = $this->con->select_db('railway');
+      $select_db = $this->con->select_db('carpart_project');
       if (!$select_db) {
         die("Failed to Select Database" . mysqli_connect_error());
       }
